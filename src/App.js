b3b07df040975b106450a6f9dbdd31c8d9db9084
Arnold Bopeto <abopeto1@gmail.com>
2020-09-19 16:52:28 +0100

    Add Facebook Login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { Layout } from './Pages/Layout'
+import React, {useState} from 'react';
+// import { Layout } from './Pages/Layout'
 import 'antd/dist/antd.min.css'
+import {Login} from "./Pages/Login";
+import {HomePage} from "./Pages/HomePage"
 
 function App() {
+  const [name, setName] = useState(null)
+
   return (
     <div className="App">
-      <Layout />
+        {
+            name ? <HomePage /> : <Login setName={setName}/>
+        }
     </div>
   );
 }
